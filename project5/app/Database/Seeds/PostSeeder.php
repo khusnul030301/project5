f3e82cbd550b35dfa5b8381d0fdd2438b7fe1f19
Khusnul Khotimah <khusnulkhotimah030301@gmail.com>
2025-07-24 01:52:39 +0700

    Modify routes and add controller-view for home, about, etc

--- project5/app/Database/Seeds/PostSeeder.php
+++ project5/app/Database/Seeds/PostSeeder.php
@@ -8,28 +8,30 @@ class PostSeeder extends Seeder
 {
     public function run()
     {
-        // Membuat data dummy
+        // membuat data
         $posts_data = [
-            [
-                'title'   => 'Mulai Nyobain Codeigniter',
-                'slug'    => 'codeigniter-starter',
-                'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore aliqua.'
-            ],
-            [
-                'title'   => 'Cara Mudah Buat Hello World',
-                'slug'    => 'hello-world',
-                'content' => 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.'
-            ],
-            [
-                'title'   => 'Meetup Komunitas Kelas Koding',
-                'slug'    => 'meetup-comunity',
-                'content' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis voluptatum deleniti atque corrupti quos.'
-            ]
+        [
+        'title' => 'Mulai Nyobain
+        Codeigniter',
+        'slug' => 'codeigniter-starter',
+        'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore aliqua.'
+        ],
+        [
+        'title' => 'Cara Mudah Buat Hello
+        World',
+        'slug' => 'hello-world',
+        'content' => 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.'
+        ],
+        [
+        'title' => 'Meetup Komunitas Kelas
+        Koding',
+        'slug' => 'meetup-comunity',
+        'content' => 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis voluptatum deleniti atque corrupti quos.'
+        ]
         ];
-
-        // Insert semua data ke tabel 'posts'
-        foreach ($posts_data as $data) {
-            $this->db->table('posts')->insert($data);
+        foreach($posts_data as $data){
+        // insert semua data ke tabel
+        $this->db->table('posts')->insert($data);
         }
     }
 }
